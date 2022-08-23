3c9ca3a53824c5cc1c8084b96be9062b66598467
krajeckas <111575369+krajeckas@users.noreply.github.com>
2022-08-23 09:17:40 +0300

    Update script.py

--- script.py
+++ script.py
@@ -2,8 +2,21 @@ from modules.configuration_handler import ConfigHandler
 from modules.initializer import Initializer
 from modules.write_to_file import WriteToFile
 from modules.tester import Tester
+import time
 
 command = ConfigHandler()
-router = Initializer().initializer()
-response = Tester().cmd_tester(command.get_param('TRM240'), router)
-WriteToFile().writetofile(response)
+router_name = Initializer().router_info()
+router = Initializer().initializer(command.get_param(router_name)['authentication'])
+# response = Tester().serial_tester(command.get_param(router_name), router, router_name)
+# WriteToFile().writetofile(response, router_name)
+stdin, stdout, stderr = router.exec_command('/etc/init.d/gsmd stop')
+stdin.close()
+print(stdout.readlines())
+stdin, stdout, stderr = router.exec_command('socat /dev/tty,raw,echo=0,escape=0x03 /dev/ttyUSB3,raw,setsid,sane,echo=0,nonblock ; stty sane\r')
+stdin.close()
+print(stdout.readlines())
+stdin, stdout, stderr = router.exec_command('ls')
+time.sleep(5)
+stdin.close()
+for line in stdout.read().splitlines():
+    print(line)
