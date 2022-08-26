c717c4e5cf401963c22e9829eff5961c00064459
krajeckas <111575369+krajeckas@users.noreply.github.com>
2022-08-26 07:40:53 +0300

    Update resulter.py

--- modules/resulter.py
+++ modules/resulter.py
@@ -1,12 +1,32 @@
-class ResultHandler:
+from colorama import Fore, Style
+from os import system
 
-    def results_pass(self, full_command, message):
-        response = ('Command: ' + full_command + ' Message: ' + message + ' Test: Passed\n')
-        print('Command: ' + full_command + ' Message: ' + message + ' Test: Passed')
+class ResultHandler:
+    __passed_count = 0
+    __failed_count = 0
+
+    def __init__(self):
+        pass
+
+    def info(self, router, test_result, command_number):
+        if test_result:
+            self.__passed_count += 1
+        else:
+            self.__failed_count += 1
+        system('clear')
+        print(Fore.BLUE + router + "\n")
+        print(Fore.GREEN + 'Passed: ' + str(self.__passed_count) + " " + Fore.RED + 'Failed: ' + str(self.__failed_count))
+        print(Style.RESET_ALL)
+        print("Number of commands: " + command_number + "\n")
+
+    def results_pass(self, full_command, message, expects):
+        full_command = full_command.replace("\"", "")
+        response = (full_command + ',' + expects + ',' + message + ',Passed\n')
+        print(full_command)
         return response
 
     def results_fail(self, full_command, message, expects):
-        response = ('Command: ' + full_command + ' Message: ' + message + ' Test: Failed Expected: ' + expects)
-        print('Command: ' + full_command + ' Message: ' + message + ' Test: Failed Expected: ' + expects)
+        full_command = full_command.replace("\"", "")
+        response = (full_command + ',' + expects + ',' + message + ',Failed\n')
+        print(full_command)
         return response
-
