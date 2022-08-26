5cff983ab1d216222f7038c335400efaa0c460d2
krajeckas <111575369+krajeckas@users.noreply.github.com>
2022-08-26 07:41:38 +0300

    Update write_to_file.py

--- modules/write_to_file.py
+++ modules/write_to_file.py
@@ -1,6 +1,15 @@
+from datetime import datetime
+import sys
+
 class WriteToFile():
-    def writetofile(self, response):
-        result_file = open("results/result.txt", "w")
-        for element in response:
-            result_file.write(element)
-        result_file.close()
+    def writetofile(self, response, router):
+        now = datetime.now()
+        filename = now.strftime("%Y-%m-%d_%H:%M:%S")
+        try:
+            result_file = open("results/" + router + "_" + filename  + ".csv", "w")
+            result_file.write('Command,Expected,Response,Test\n')
+            for element in response:
+                result_file.write(element)
+            result_file.close()
+        except FileNotFoundError:
+            print('results directory is missing')
