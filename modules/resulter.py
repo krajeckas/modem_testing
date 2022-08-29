76e75574d5688a6388d934acfd671fb5b902fd5b
krajeckas <111575369+krajeckas@users.noreply.github.com>
2022-08-29 08:21:09 +0300

    Update resulter.py

--- modules/resulter.py
+++ modules/resulter.py
@@ -8,25 +8,30 @@ class ResultHandler:
     def __init__(self):
         pass
 
-    def info(self, router, test_result, command_number):
+    def info(self, router, command_number):
+        system('clear')
+        print(Fore.BLUE + router)
+        print(Style.RESET_ALL)
+        print("Number of commands: " + command_number + "\n")
+
+    def results_pass(self, full_command, message, expects, test_result):
         if test_result:
             self.__passed_count += 1
         else:
             self.__failed_count += 1
-        system('clear')
-        print(Fore.BLUE + router + "\n")
-        print(Fore.GREEN + 'Passed: ' + str(self.__passed_count) + " " + Fore.RED + 'Failed: ' + str(self.__failed_count))
-        print(Style.RESET_ALL)
-        print("Number of commands: " + command_number + "\n")
-
-    def results_pass(self, full_command, message, expects):
         full_command = full_command.replace("\"", "")
         response = (full_command + ',' + expects + ',' + message + ',Passed\n')
-        print(full_command)
+        print(Fore.GREEN + 'Passed: ' + str(self.__passed_count) + " " + Fore.RED + 'Failed: ' + str(self.__failed_count))
+        print(Style.RESET_ALL)
         return response
 
-    def results_fail(self, full_command, message, expects):
+    def results_fail(self, full_command, message, expects, test_result):
+        if test_result:
+            self.__passed_count += 1
+        else:
+            self.__failed_count += 1
         full_command = full_command.replace("\"", "")
         response = (full_command + ',' + expects + ',' + message + ',Failed\n')
-        print(full_command)
+        print(Fore.GREEN + 'Passed: ' + str(self.__passed_count) + " " + Fore.RED + 'Failed: ' + str(self.__failed_count))
+        print(Style.RESET_ALL)
         return response
